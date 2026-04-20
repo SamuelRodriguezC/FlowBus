9c7f0bd9f68c927653a99ec0cd500f15b58c7b16
SamuelRodríguezC <samuelrc250@gmail.com>
2026-04-19 20:41:58 -0500

    Serialización de usuarios para permitir el registro desde la API

--- Backend/flowbusApi/bookings/serializers.py
+++ Backend/flowbusApi/bookings/serializers.py
@@ -1,6 +1,23 @@
 from rest_framework import serializers # type: ignore
 from .models import Bus, Seat
+from django.contrib.auth.models import User 
 
+# Sreializador de usuarios para permirit el registro desde la API
+class UserSerializer(serializers.ModelSerializer): 
+    password = serializers.CharField(write_only=True) # La contraseña no se expone en las respuestas 
+    
+    class Meta: 
+        model = User
+        fields = ['username', 'email', 'password']
+        
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username= validated_data['username'],
+            email = validated_data['email'],
+            password = validated_data['password']
+        )
+        return user
+    
 class BusSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Bus
