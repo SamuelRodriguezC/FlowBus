c23feddb57aa1f1b4f6028de105038be1704554a
SamuelRodríguezC <samuelrc250@gmail.com>
2026-04-21 17:13:06 -0500

    Ajustar url para mostrar las reservas que tiene cada usuario

--- Backend/flowbusApi/bookings/urls.py
+++ Backend/flowbusApi/bookings/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('buses/', BusListCreateView.as_view(), name = 'busList'),
     path('register/', RegisterView.as_view(), name = 'register'),
     path('login/', LoginView.as_view(), name = 'login'),
-    path('user-bookings/', UserBookingView.as_view(), name = 'user-bookings'),
+    path('user/<int:user_id>/bookings/', UserBookingView.as_view(), name = 'user-bookings'),
     path('booking/', BookingView.as_view(), name = 'bookings'),
     
 ]
